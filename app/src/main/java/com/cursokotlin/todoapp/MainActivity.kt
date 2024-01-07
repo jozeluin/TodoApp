95bec48b5c6e92bf2c023e02ac97efdde81b1b00
jozel <jozeluin@gmail.com>
2024-01-07 20:04:29 +0100

    Pc.Dialogo Creador de tareas. Creamos el Floatinbutton con su Dialogo

--- app/src/main/java/com/cursokotlin/todoapp/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/todoapp/MainActivity.kt
@@ -3,14 +3,21 @@ package com.cursokotlin.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.cursokotlin.todoapp.addtasks.ui.TasksScreen
+import com.cursokotlin.todoapp.addtasks.ui.TasksViewModel
 import com.cursokotlin.todoapp.ui.theme.TodoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val tasksViewModel :TasksViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -20,6 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                   // TasksScreen(tasksViewModel)
                     TasksScreen()
                 }
             }
--- app/src/main/java/com/cursokotlin/todoapp/addtasks/ui/TasksScreen.kt
+++ app/src/main/java/com/cursokotlin/todoapp/addtasks/ui/TasksScreen.kt
@@ -1,10 +1,90 @@
 package com.cursokotlin.todoapp.addtasks.ui
 
+//import androidx.compose.material3.FloatingActionButton
+// import androidx.compose.material3.FloatingActionButton
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+@Preview
+@Composable
+fun TasksScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        AddTasksDialog(true, onDismiss = {}, onTaskAdded = {})
+        FabDialog(Modifier.align(Alignment.BottomEnd))
+    }
+
+
+}
 
 @Composable
-fun TasksScreen(){
+fun FabDialog(modifier: Modifier) {
+    FloatingActionButton(onClick = {
+        //mostrar dialogo
+    }, modifier = modifier.padding(16.dp)) {
 
+        Icon(Icons.Filled.Add, contentDescription = "null")
+
+    }
+}
+
+@Composable
+fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit,onTaskAdded:(String)->Unit) {
+    var myTask by remember { mutableStateOf("") }
+
+    if (show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Añade tu tarea",
+                    fontSize = 18.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                TextField(
+                    value = myTask, onValueChange = { myTask = it }, singleLine = true, maxLines = 1
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(onClick = {
+                    onTaskAdded(myTask)
+                }, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Añadir Tarea")
+
+                }
+            }
+        }
+    }
 
 }
 
