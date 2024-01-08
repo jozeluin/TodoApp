fe96e96d0de04b6553d8d6a29bfaae703faab522
jozel <jozeluin@gmail.com>
2024-01-08 12:44:53 +0100

    Pc.conigurando Tareas, tasks

--- app/src/main/java/com/cursokotlin/todoapp/addtasks/ui/TasksScreen.kt
+++ app/src/main/java/com/cursokotlin/todoapp/addtasks/ui/TasksScreen.kt
@@ -5,12 +5,16 @@ package com.cursokotlin.todoapp.addtasks.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Checkbox
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -27,6 +31,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -35,20 +40,57 @@ import androidx.compose.ui.window.Dialog
 @Composable
 fun TasksScreen(tasksViewModel: TasksViewModel) {
 
-    val showDialog:Boolean by tasksViewModel.showDialog.observeAsState(false)
+    val showDialog: Boolean by tasksViewModel.showDialog.observeAsState(false)
 
     Box(modifier = Modifier.fillMaxSize()) {
-        AddTasksDialog(showDialog, onDismiss = {tasksViewModel.onDialogClose()}, onTaskAdded = {tasksViewModel.onTaskCreated(it)})
-        FabDialog(Modifier.align(Alignment.BottomEnd),tasksViewModel)
+        ItemTask()
+        AddTasksDialog(
+            showDialog,
+            onDismiss = { tasksViewModel.onDialogClose() },
+            onTaskAdded = { tasksViewModel.onTaskCreated(it) })
+        FabDialog(Modifier.align(Alignment.BottomEnd), tasksViewModel)
+        TasksList(tasksViewModel)
     }
 
 
 }
 
 @Composable
+fun TasksList(tasksViewModel: TasksViewModel) {
+    LazyColumn {
+
+    }
+}
+
+@Preview
+@Composable
+fun ItemTask() {
+
+    Card(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        // border = BorderStroke(2.dp, Color.Magenta),
+        elevation = 8.dp
+    ) {
+        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = "Ejemplo", modifier = Modifier.padding(horizontal = 4.dp)
+                    .weight(1f)
+
+
+            )
+            Checkbox(checked = true, onCheckedChange = {})
+        }
+    }
+
+}
+
+
+@Composable
 fun FabDialog(modifier: Modifier, tasksViewModel: TasksViewModel) {
     FloatingActionButton(onClick = {
-       tasksViewModel.onShowDialogClick()
+        tasksViewModel.onShowDialogClick()
     }, modifier = modifier.padding(16.dp)) {
 
         Icon(Icons.Filled.Add, contentDescription = "null")
@@ -57,7 +99,7 @@ fun FabDialog(modifier: Modifier, tasksViewModel: TasksViewModel) {
 }
 
 @Composable
-fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit,onTaskAdded:(String)->Unit) {
+fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) -> Unit) {
     var myTask by remember { mutableStateOf("") }
 
     if (show) {
