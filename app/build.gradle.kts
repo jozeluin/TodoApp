4d57f30e42f5a0250da146b320444925abe07dc6
JozeLuin <jozeluin@gmail.com>
2024-01-09 19:27:19 +0100

    Asus. Implementado Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,9 +70,13 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     //Dagger Hilt
-    implementation ("com.google.dagger:hilt-android:2.41")
-    kapt ("com.google.dagger:hilt-android-compiler:2.41")
+    implementation ("com.google.dagger:hilt-android:2.44")
+    kapt ("com.google.dagger:hilt-android-compiler:2.44")
     //LiveData
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
+    //Room
+    implementation ("androidx.room:room-runtime:2.5.0")
+    implementation ("androidx.room:room-ktx:2.5.0")
+    kapt("androidx.room:room-compiler:2.5.0")
 }
